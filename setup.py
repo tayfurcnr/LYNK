080b7828644284f34aaf909d8739ff438a07d9b7
TAYFUR CINAR <tayfurcnr@gmail.com>
2026-03-30 20:47:57 +0300

    package update

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ if __name__ == "__main__":
             'pyserial',
             'crcmod',
             'PyYAML',
-            'protobuf',
+            'protobuf<3.21',
             'lz4',
             'fastapi',
             'uvicorn'
